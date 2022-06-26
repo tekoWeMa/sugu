38d007723a08c1ff5ef928ae21b38a9d9b4f7a96
tekoWeMa <mario.weilenmann@edu.teko.ch>
2022-06-26 11:31:21 +0200

    first commit

---
+++ src/main/java/ch/wema/Main.java
@@ -0,0 +1,27 @@
+package ch.wema;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+
+public class Main {
+
+
+    public static void main(final String[] args) {
+        final String token = "OTU4NDYzNTc3NzY3OTYwNTk2.GXCvQm.WtsvCNCaY6DPryl2K4UuNLuYYEveAAUhz1tcvI";
+        final DiscordClient client = DiscordClient.create(token);
+        final GatewayDiscordClient gateway = client.login().block();
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage("Pong!").block();
+            }
+        });
+
+        gateway.onDisconnect().block();
+    }
+}
