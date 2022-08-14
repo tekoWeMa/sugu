e0405616677818894ca7daa6a56aa0f3aac1e9ea
sirh3e <sirh3e@protonmail.com>
2022-08-14 14:06:36 +0200

    refactored PING command, added YEP reaction

--- src/main/java/ch/wema/Main.java
+++ src/main/java/ch/wema/Main.java
@@ -1,31 +1,34 @@
 package ch.wema;
 
-import discord4j.core.DiscordClient;
-import discord4j.core.GatewayDiscordClient;
+import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.MessageChannel;
 
 public class Main {
 
 
     public static void main(final String[] args) {
         final var token = System.getenv("DISCORD_CLIENT_TOKEN");
-        final DiscordClient client = DiscordClient.create(token);
-        final GatewayDiscordClient gateway = client.login().block();
+        final var client = DiscordClientBuilder.create(token).build().login().block();
 
-        gateway.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();
-            if ("!ping".equals(message.getContent())) {
-                final MessageChannel channel = message.getChannel().block();
-                channel.createMessage("Pong!").block();
-            }
-            if ("!version".equals(message.getContent())){
-                final MessageChannel channel = message.getChannel().block();
-                channel.createMessage("New version").block();
-            }
-        });
+        //PING command
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .subscribe(); //ToDo latency
 
-        gateway.onDisconnect().block();
+        //YEP reaction
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().toLowerCase().contains("yep"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("YEP"))
+                .subscribe();
+
+        client.onDisconnect().block();
     }
 }
