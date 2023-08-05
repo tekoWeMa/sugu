8e617df7818f1995380d8902cb78cadb7d442594
Mario Weilenmann <98219925+tekoWeMa@users.noreply.github.com>
2023-08-05 22:42:04 +0200

    Add Activity Details to PresenceUpdateEvent

--- src/main/java/ch/wema/loggers/UserStatusLogger.java
+++ src/main/java/ch/wema/loggers/UserStatusLogger.java
@@ -5,6 +5,7 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.entity.User;
 import discord4j.common.util.Snowflake;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.object.presence.Activity;
 import reactor.core.publisher.Mono;
 
 
@@ -31,8 +32,40 @@ public class UserStatusLogger {
                 .flatMap(user -> {
                     // Extract the user's status from their status
                     String status = event.getCurrent().getStatus().toString();
-                    String content = "The Status of the user " + user.getUsername() + " (" + user.getId().asString() + ") changed to " + status + ".";
-                    return logChannel.createMessage(content);
+
+                    // Build the base log message
+                    StringBuilder content = new StringBuilder("The Status of the user " + user.getUsername() + " (" + user.getId().asString() + ") changed to " + status + ".");
+
+                    // If the user has any activities, append them to the log message
+                    if (!event.getCurrent().getActivities().isEmpty()) {
+                        content.append("\nActivities:");
+                        for (Activity activity : event.getCurrent().getActivities()) {
+                            content.append("\n- Name: ").append(activity.getName())
+                                    .append("\n- Type: ").append(activity.getType().name());
+
+                            if (activity.getDetails().isPresent()) {
+                                content.append("\n- Details: ").append(activity.getDetails().get());
+                            }
+
+                            if (activity.getState().isPresent()) {
+                                content.append("\n- State: ").append(activity.getState().get());
+                            }
+
+                            if (activity.getStart().isPresent()) {
+                                content.append("\n- Start: ").append(activity.getStart().get().toString());
+                            }
+
+                            if (activity.getEnd().isPresent()) {
+                                content.append("\n- End: ").append(activity.getEnd().get().toString());
+                            }
+
+                            if (activity.getApplicationId().isPresent()) {
+                                content.append("\n- Application ID: ").append(activity.getApplicationId().get().asString());
+                            }
+                        }
+                    }
+
+                    return logChannel.createMessage(content.toString());
                 })
                 .then();
     }
